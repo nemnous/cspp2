54c452e3f8bdf669be599ec6c3f89edcdd989600
nemnous <nani.botta.123@msitprogram.net>
2018-08-28 17:25:54 +0530

    Hello World

---
+++ cspp2-practice/m1/a10/Helloworld.java
@@ -0,0 +1,23 @@
+/**
+ * author: @nemnous.
+ * Date: 27 Aug 2018
+ * Hello World
+ */
+public final class Helloworld {
+    /**
+     * Constructs the object.
+     */
+    private Helloworld() {
+        /**
+         * does nothing.
+         */
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+}
