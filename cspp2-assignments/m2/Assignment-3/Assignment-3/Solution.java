d02938e5a7b1fac6db6d40584dc620519aeef0eb
nemnous <nani.botta.123@msitprogram.net>
2018-08-28 22:41:34 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Assignment-3/Solution.java
@@ -8,29 +8,34 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		/**
-		 * does nothing
-		 */
-	}
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);      
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * does nothing
+         */
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
         long result=power(base,exponent);
         System.out.println(result);
-	}
-	static long power(int base, int exponent) {
-		long product = 1;
-		for(int i = 0; i <exponent; i++) {
-			product *= base;
-		}
-		return product;
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
+    }
+    static long power(int base, int exponent) {
+        long product = 1;
+        for(int i = 0; i <exponent; i++) {
+            product *= base;
+        }
+        return product;
+    }
+    /*
+    Need to write the power function and print the output.
+    */
 }
