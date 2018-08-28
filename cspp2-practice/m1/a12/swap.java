47434f32fa70cf38fd098ce175a29850be32e0d6
nemnous <nani.botta.123@msitprogram.net>
2018-08-28 10:27:51 +0530

    swap after sun check

--- cspp2-practice/m1/a12/swap.java
+++ cspp2-practice/m1/a12/swap.java
@@ -1,15 +1,31 @@
+/**.
+    * author: @nemnous.
+    * Date: 27 Aug 18.
+    * Swapping two numbers.
+*/
 import java.util.Scanner;
-public class swap{
-	public static void main(String[] args) {
-		{
-			System.out.println("Enter a and b:");
-			Scanner inp = new Scanner(System.in);
-			int a = inp.nextInt();
-			int b = inp.nextInt();
-			int t = a;
-			a = b;
-			b = t;
-			System.out.println(a + "," + b);
-		}
-	}
-}
+
+/**
+    *class swap.
+*/
+public final class Swap {
+    /**
+        * private constructor.
+    */
+    private Swap() {
+    }
+    /**
+        * This is main.
+        * @param args is arguments
+    */
+    public static void main(final String[] args) {
+            System.out.println("Enter a and b:");
+            Scanner inp = new Scanner(System.in);
+            int a = inp.nextInt();
+            int b = inp.nextInt();
+            int t = a;
+            a = b;
+            b = t;
+            System.out.println(a + "," + b);
+        }
+    }
