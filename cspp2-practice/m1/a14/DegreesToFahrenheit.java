342529faba228ccc6eac65e145747a7cf4ee244c
nemnous <nani.botta.123@msitprogram.net>
2018-08-28 11:45:46 +0530

    degree to fahrenheit

---
+++ cspp2-practice/m1/a14/DegreesToFahrenheit.java
@@ -0,0 +1,29 @@
+/**
+    * author: @nemnous.
+    * Date: 27 Aug 2018
+    * Degrees to Fahreinheit conversion
+*/
+import java.util.Scanner;
+/**
+    * this is main class.
+*/
+
+public final class DegreesToFahrenheit {
+    /**
+        * this is private constructor.
+    */
+    private DegreesToFahrenheit() {
+
+    }
+    /**
+        * @param args is an argument.
+    */
+    public static void main(final String[] args) {
+        System.out.println("Enter temperature in celsius:");
+        Scanner inp = new Scanner(System.in);
+        float celsius = inp.nextFloat();
+        final float t1 = 1.8;
+        final int t2 = 32;
+        System.out.println(celsius * t1 + t2);
+    }
+}
