5887b3cb819d938d827f034b6b89aee7e51d72a5
nemnous <nani.botta.123@msitprogram.net>
2018-08-29 14:26:47 +0530

    loops

---
+++ cspp2-practice/m3/a6/ConverttoWhileLoop.java
@@ -0,0 +1,35 @@
+/**
+ * author: @nemnous.
+ * Date: 29 Aug 2018
+ * Convert the following to While Loop:
+ * System.out.println(2);
+ * System.out.println(4);
+ * System.out.println(6);
+ * System.out.println(8);
+ * System.out.println(10);
+ * System.out.println(GoodBye!)
+ */
+public final class ConverttoWhileLoop {
+    /**
+     * Constructs the object.
+     */
+    private ConverttoWhileLoop() {
+        /**
+         * does nothing.
+         */
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        int i = 2;
+        final int ten = 10;
+        while (i <= ten) {
+            System.out.println(i);
+            i += 2;
+        }
+        System.out.println("GoodBye");
+    }
+}
