9fc196fdec84ad100843525601b0538bda046d02
nemnous <nani.botta.123@msitprogram.net>
2018-08-29 14:43:28 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- cspp2-assignments/m3/Assignment-3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Assignment-3/Solution.java
@@ -1,28 +1,53 @@
-
+/**
+ * author: @nemnous.
+ * Date: 29Aug2018
+ * GCD of two numbers
+ */
 import java.util.Scanner;
-/*
-	Do not modify this main function.
-	*/
-public class Solution {
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * does nothing.
+         */
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        int gcd = gcd(n1,n2);
+        int gcd = gcd(n1, n2);
         System.out.println(gcd);
     }
-    static int gcd(int n1, int n2) {
-    	int gcd = 0;
-    	for (int i = 1; i <= n1 && i <= n2; i++) {
-    		if(n1 % i == 0 && n2 % i == 0) {
-    			gcd = i;
-    		}
-    	}
-    	return gcd;
+    /**
+     * gcd function.
+     *
+     * @param      n1    The n 1
+     * @param      n2    The n 2
+     *
+     * @return     returns gcd
+     */
+    static int gcd(final int n1, final int n2) {
+        int gcd = 0;
+        for (int i = 1; i <= n1 && i <= n2; i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
     }
     /*
-	Need to write the gcd function and print the output.
-	*/
+    Need to write the gcd function and print the output.
+    */
 }
