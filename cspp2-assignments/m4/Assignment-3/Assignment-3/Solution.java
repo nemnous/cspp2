be499f03a54ad8a87d1da589f1e789f068840008
nemnous <nani.botta.123@msitprogram.net>
2018-08-30 20:13:49 +0530

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-3/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Assignment-3/Solution.java
@@ -1,32 +1,55 @@
-
-// Given an String, Write a java method that returns the decimal value for the given binary string.
+/**
+ * author: @nemnous.
+ * Date 30 Aug 2018
+ * Binary to decimal
+ */
 
 import java.util.Scanner;
-public final class Solution
-{/*
-	Do not modify this main function.
-	*/
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		// sc.nextLine();
-		for (int i = 0; i < n; i++) {
-			String s = sc.next();
-			// System.out.println(s);
-			String res=binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	static String binaryToDecimal(String s) {
-		int base = 1;
-		int sum = 0;
-		for(int i = s.length() - 1; i >= 0 ; i--) {
-			sum = sum + (base * Character.getNumericValue(s.charAt(i)));
-			base *= 2;
-		}
+/**
+ * class Solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * unused constructor.
+         */
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            // System.out.println(s);
+            String res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+    /**
+     * binary to decimal function.
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    static String binaryToDecimal(final String s) {
+        int base = 1;
+        int sum = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sum = sum + (base * Character.getNumericValue(s.charAt(i)));
+            base *= 2;
+        }
 
-		return Integer.toString(sum);
-	}
+        return Integer.toString(sum);
+    }
 
 
 }
