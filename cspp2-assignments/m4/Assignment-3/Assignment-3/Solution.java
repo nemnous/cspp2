90a4ce278559d9871464c7b26ae351595c61d6a8
nemnous <nani.botta.123@msitprogram.net>
2018-08-30 14:38:31 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-3/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Assignment-3/Solution.java
@@ -9,6 +9,7 @@ public final class Solution
 	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		String sss = sc.next();
 		for (int i = 0; i < n; i++) {
 			String s = sc.nextLine();
 			// System.out.println(s);
