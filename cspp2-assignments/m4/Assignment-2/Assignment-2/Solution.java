ab6ddfb5acae5cd087b80475f7711b1142307e5c
nemnous <nani.botta.123@msitprogram.net>
2018-08-30 19:52:00 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
@@ -1,36 +1,54 @@
+/**
+ * author: @nemnous.
+ * Date: 30 Aug 2018
+ * Addition of matrices
+ */
 import java.util.Scanner;
-public class Solution {
-	/* Fill the main function to print resultant of addition of matrices*/
-	public static void main(String[] args) {
-		Scanner inp = new Scanner(System.in);
-		int row1 = Integer.parseInt(inp.next());
-		int col1 = Integer.parseInt(inp.next());
-		int[][] arr1 = new int[row1][col1];
-		for (int i = 0; i < row1; i++) {
-			for (int j = 0;j < col1; j++) {
-				arr1[i][j] = Integer.parseInt(inp.next());
-			}
-		}
-		int row2 = Integer.parseInt(inp.next());
-		int col2 = Integer.parseInt(inp.next());
-		int[][] arr2 = new int[row2][col2];
-		for (int i = 0; i < row2; i++) {
-			for (int j = 0;j < col2; j++) {
-				arr2[i][j] = Integer.parseInt(inp.next());
-			}
-		}
-		if (row1 == row2 && col1 == col2){
-			for (int i = 0; i < row2; i++) {
-			for (int j = 0;j < col2; j++) {
-				String b = (j != col2-1)? " ":"";
-				System.out.print(arr1[i][j] + arr2[i][j] + b);
-			}
-			System.out.println("");
-		}	
-		}
-		else {
-			System.out.println("not possible");
-		}
-		
-	}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * unused constructor.
+         */
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int row1 = Integer.parseInt(inp.next());
+        int col1 = Integer.parseInt(inp.next());
+        int[][] arr1 = new int[row1][col1];
+        for (int i = 0; i < row1; i++) {
+            for (int j = 0; j < col1; j++) {
+                arr1[i][j] = Integer.parseInt(inp.next());
+            }
+        }
+        int row2 = Integer.parseInt(inp.next());
+        int col2 = Integer.parseInt(inp.next());
+        int[][] arr2 = new int[row2][col2];
+        for (int i = 0; i < row2; i++) {
+            for (int j = 0; j < col2; j++) {
+                arr2[i][j] = Integer.parseInt(inp.next());
+            }
+        }
+        if (row1 == row2 && col1 == col2) {
+            for (int i = 0; i < row2; i++) {
+            for (int j = 0; j < col2; j++) {
+                String b = (j != col2 - 1) ? " " : "";
+                System.out.print(arr1[i][j] + arr2[i][j] + b);
+            }
+            System.out.println("");
+        }
+        } else {
+            System.out.println("not possible");
+        }
+    }
 }
