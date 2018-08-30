fdb02ecea6451e793ee89980f064c740a4c78a60
nemnous <nani.botta.123@msitprogram.net>
2018-08-30 11:06:47 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
@@ -22,9 +22,9 @@ public class Solution {
 		if (row1 == row2 && col1 == col2){
 			for (int i = 0; i < row2; i++) {
 			for (int j = 0;j < col2; j++) {
-				System.out.print(arr1[i][j] + arr2[i][j] + " ");
+				String b = (j != col2-1)? " ":"";
+				System.out.print(arr1[i][j] + arr2[i][j] + b);
 			}
-			System.out.print('\b');
 			System.out.println("");
 		}	
 		}
