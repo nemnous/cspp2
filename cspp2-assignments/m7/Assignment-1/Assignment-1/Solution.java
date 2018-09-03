8260281ce0ef492c02e42e2c88d3d18818d49caa
nemnous <nani.botta.123@msitprogram.net>
2018-09-04 00:18:35 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m7/Assignment-1/Assignment-1/Solution.java
@@ -0,0 +1,58 @@
+/**
+ * author: @nemnous.
+ * Date: 03 Sep 2018
+ * Input validator
+ */
+import java.util.Scanner;
+/**
+ * Class for input validator.
+ */
+class InputValidator {
+    /**
+     * private String.
+     */
+    private String str1;
+    /**
+     * Constructs the object.
+     *
+     * @param      str   The string
+     */
+    InputValidator(final String str) {
+        str1 = str;
+    }
+    /**
+     * validate data.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    boolean validateData() {
+        final int six = 6;
+        return (str1.length() >= six);
+    }
+}
+/**
+ * Class for solution.
+ */
+final public class Solution {
+    /**
+     * Solution.
+     */
+    private Solution() {
+        /**
+         * unused constructor.
+         */
+    }
+    
+    /**
+     * main func.
+     * @param args is argument.
+     */
+    public static void main(final String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);       
+        System.out.println(i.validateData());
+    }
+
+}
