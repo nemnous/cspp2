fd5653305fde00f96d572f66f7058d2e428b409f
nemnous <nani.botta.123@msitprogram.net>
2018-09-11 15:56:36 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m15/Assignment-1/Assignment-1/Solution.java
+++ cspp2-assignments/m15/Assignment-1/Assignment-1/Solution.java
@@ -350,17 +350,20 @@ class List {
      */
     public List subList(final int start, final int end) {
         // write the logic for subList
-        if (start >= end || (start < 0 || end < 0) || size == 0) {
-            System.out.println("Index Out of Bounds Exception");
-            return null;
-        }
-
+        // if (start >= end || (start < 0 || end < 0) || size == 0) {
+        //     System.out.println("Index Out of Bounds Exception");
+        //     return null;
+        // }
         List slist = new List();
 
-        for (int i = start; i < end; i++) {
-            slist.add(array[i]);
+        try {
+            for (int i = start; i < end; i++) {
+                slist.add(array[i]);
+            }
         }
-
+        catch (IndexOutOfBoundsException E) {
+            System.out.println("Index Out of Bounds Exception");
+        }
         return slist;
     }
 
