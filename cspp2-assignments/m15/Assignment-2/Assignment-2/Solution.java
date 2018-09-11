d30bb1a8849f1551cbb8f8ba52ea8e320175913f
nemnous <nani.botta.123@msitprogram.net>
2018-09-11 22:16:30 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m15/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m15/Assignment-2/Assignment-2/Solution.java
@@ -307,217 +307,111 @@ public final class Solution {
      * @param      args  The arguments
 
      */
-
     public static void main(final String[] args) {
-
         // instantiate this set
-
         SortedSet s = new SortedSet();
-
         // code to read the test cases input file
-
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
-
         // check if there is one more line to process
-
         while (stdin.hasNext()) {
-
             // read the line
-
             String line = stdin.nextLine();
-
             // split the line using space
-
             String[] tokens = line.split(" ");
-
             // based on the list operation invoke the corresponding method
-
             switch (tokens[0]) {
-
             case "size":
-
                 System.out.println(s.size());
-
                 break;
-
             case "contains":
-
                 System.out.println(s.contains(Integer.parseInt(tokens[1])));
-
                 break;
-
             case "print":
-
                 System.out.println(s);
-
                 break;
-
             case "add":
-
                 int[] intArray = intArray(tokens[1]);
-
                 if (intArray.length == 1) {
-
                     s.add(intArray[0]);
-
                 } else {
-
                     s.add(intArray);
-
                 }
-
                 break;
-
             case "intersection":
-
                 s = new SortedSet();
-
                 Set t = new Set();
-
                 intArray = intArray(tokens[1]);
-
                 s.add(intArray);
-
                 intArray = intArray(tokens[2]);
-
                 t.add(intArray);
-
                 System.out.println(s.intersection(t));
-
                 break;
-
             case "retainAll":
-
                 s = new SortedSet();
-
                 intArray = intArray(tokens[1]);
-
                 s.add(intArray);
-
                 intArray = intArray(tokens[2]);
-
                 System.out.println(s.retainAll(intArray));
-
                 break;
-
             case "cartesianProduct":
-
                 s = new SortedSet();
-
                 t = new Set();
-
                 intArray = intArray(tokens[1]);
-
                 s.add(intArray);
-
                 intArray = intArray(tokens[2]);
-
                 t.add(intArray);
-
                 System.out.println(Arrays.deepToString(s.cartesianProduct(t)));
-
                 break;
-
             case "subSet":
-
                 try {
-
                     if (tokens.length != 2) {
-
                         break;
-
                     }
-
                     String[] arrstring3 = tokens[1].split(",");
-
                     int[] object = s.subSet(Integer.parseInt(arrstring3[0]),
-
                                             Integer.parseInt(arrstring3[1]));
-
                     if (object != null) {
-
                         System.out.println(Arrays.toString(object).replace("[",
-
                                            "{").replace("]", "}"));
-
                     }
-
                 } catch (Exception e) {
-
                     System.out.println(e.getMessage());
-
                 }
-
                 break;
-
             case "headSet":
-
                 try {
-
                     if (tokens.length != 2) {
-
                         break;
-
                     }
-
                     int[] obj = s.headSet(Integer.parseInt(tokens[1]));
-
                     if (obj != null) {
-
                         System.out.println(Arrays.toString(obj).replace("[",
-
                                            "{").replace("]", "}"));
-
                     }
-
                 } catch (Exception e) {
-
                     System.out.println(e.getMessage());
-
                 }
-
                 break;
-
             case "last":
-
                 try {
-
                     if (tokens.length != 1) {
-
                         break;
-
                     }
-
                     int temp = s.last();
-
                     System.out.println(temp);
-
                 } catch (Exception e) {
-
                     System.out.println(e.getMessage());
-
                 }
-
                 break;
-
             case "addAll":
-
                 int[] intArr = intArray(tokens[1]);
-
                 if (intArr.length == 1) {
-
                     s.add(intArr[0]);
-
                 } else {
-
                     s.add(intArr);
-
                 }
-
                 break;
-
             default:
-
                 break;
 
             }
